7ace3c58cb2111023776873367ce2fc0a46fe4bc
LimYooyeol <duf7040@naver.com>
2023-05-09 11:10:21 +0900

    Initial commit

---
+++ src/test/java/com/test/fasoo/FasooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.fasoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
